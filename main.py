b7df41e0f9ac031afd8c58f7a633d97ff1eefea1
Yarmorkin Timofey <132182049+MPE0111212@users.noreply.github.com>
2025-11-28 20:10:28 +0700

    Update main.py
    part 1

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
 import logging as nonflask_logging
+from constants import *
 from random import randint
 from markupsafe import Markup
 from flask import *
+import openpyxl
 import datetime
 import qrcode
-import re
-
-menu = {"Маргарита": 1100, "Пепперони не острая": 1000, "Тесто": 100,
-        "Пицца с ананасами": 900, "Вода": 100, "Сок": 200, "Кидс": 950, "Мясная пицца": 1010, "Деревенская пицца": 1050,
-        "Сырная пицца": 1199, "Гавайская пицца": 1259}
-spec_menu = {"Маргарита с весом 100 тонн": 5000, "Газировка": 300, "Пепперони очень острая": 1100, "Лимонад": 259}
-ingredients = {"Колбаса": 100, "Грибы": 120, "Огурцы": 80, "Кетчуп": 40, "Майонез": 30, "Сыр": 50, "Оливки": 20,
-               "Горчица": 90, "Бекон": 110, "Помидоры": 70, "Лук": 30, "Шпинат": 40}
+import bcrypt
+import os
 
 app = Flask(__name__)
-app.run(threaded=True)
 app.logger.setLevel(nonflask_logging.DEBUG)
 
-app.secret_key = b'qEfCC[20s=n\d,w/q1;D'
+app.secret_key = os.urandom(24)
 
-user_info_string = lambda: (f'Номер заказа: {session.get("order_number")}\nИмя: {session.get("name")}\nФамилия:{session.get("surname")}\nОтчество: {session.get("lastname")}\n'
+user_info_string = lambda: (f'Номер заказа: {session.get("order_number")}\nИмя: {session.get("name")}\nФамилия: {session.get("surname")}\nОтчество: {session.get("lastname")}\n'
     f'Возраст: {session.get("age")}\nАдрес электронной почты: {session.get("email")}\n'
     f'Телефонный номер: {session.get("phone")}')
-
+purchases_converted_to_string = lambda: ''.join([f"## {v[1]} {k} ({v[0]} руб/шт) = {v[0] * v[1]} итого ## " for k, v in session.get("purchases").items()])
 
 def update_purchases_data():
     session['total_cost'] = 0
@@ -37,25 +32,26 @@ def nl2br_filter(s):
     return Markup(s.replace('\n', '<br>\n'))
 
 
-def test_info(name, surname, lastname, age, phone_number, email):
-    if not name or not surname or not lastname or not age or not phone_number or not email:
+#  Рекурсия, потому что нужна обязательно по ТЗ
+def test_info(name, surname, lastname, age, phone_number, email, *, entry_index=0):
+    if entry_index > 4:
+        if name != name.capitalize():
+            session["name"] = name.capitalize()
+        if surname != surname.capitalize():
+            session["surname"] = surname.capitalize()
+        if lastname != lastname.capitalize():
+            session["lastname"] = lastname.capitalize()
+        try:
+            if not 0 < int(age) < 140:
+                session["age"] = 1
+        except:
+            session["age"] = 1
+        return True
+    current_test = (test_name, test_surname, test_lastname, test_phone, test_email)[entry_index]
+    current_arg = (name, surname, lastname, phone_number, email)[entry_index]
+    if not current_test(current_arg):
         return False
-    if (not re.fullmatch(r'[А-Яа-яЁё-]+', name) or not re.fullmatch(r'[А-Яа-яЁё-]+', surname) or not re.fullmatch(r'[А-Яа-яЁё-]+', lastname)
-            or not re.fullmatch(r'^[\d\s\-\(\)\+]+$', phone_number)
-            or not re.fullmatch(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email)):
-        return False
-    if name != name.capitalize():
-        name = name.capitalize()
-    if surname != surname.capitalize():
-        surname = surname.capitalize()
-    if lastname != lastname.capitalize():
-        lastname = lastname.capitalize()
-    try:
-        if not 0 < int(age) < 140:
-            age = 1
-    except:
-        age = 1
-    return True
+    return test_info(name, surname, lastname, age, phone_number, email, entry_index=entry_index + 1)
 
 
 def qr_code():
@@ -64,28 +60,127 @@ def qr_code():
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=1,
-        border=3,
+        border=1,
     )
     qr.add_data(qr_url)
     qr.make(fit=True)
 
     img = qr.make_image(fill_color="black", back_color="white")
-    qr_filename = f"qrcode{session.get('order_number')}.png"
-    img.save(qr_filename)
+    qr_filename = f"qrcode.png"
+    img.save(os.path.join(app.static_folder, qr_filename))
     return qr_filename
+
+
+def edit_excel(*args, name=ORDERS_XLSX_PATH, method=APPEND):
+    if method == APPEND:  # Добавить строку в таблицу и создать таблицу, если её не существует
+        try:
+            wb = openpyxl.load_workbook(name)
+            ws = wb['Sheet1']
+        except:
+            wb = openpyxl.Workbook()
+            ws = wb.active
+            ws.title = 'Sheet1'
+        ws.append(args)
+        try:
+            wb.save(name)
+        except Exception as e:
+            app.logger.error(
+                f"Таблица Excel '{name}' не была закрыта перед сохранением. Заказ не сохранён в таблицу. ({e})")
+    elif method == MODIFY:  # Изменить значение в таблице по строке и столбцу на значение val (операция сложения)
+        if len(args) != 3:
+            app.logger.error(f"Количество аргументов для изменения значения не равно 3 (индекс строки, индекс столбца, значение)")
+            return
+        row, col, val = args[0], args[1], args[2]
+        try:
+            wb = openpyxl.load_workbook(name)
+            ws = wb['Sheet1']
+            if ws.cell(row, col).value is None:
+                ws.cell(row, col).value = val
+            else:
+                ws.cell(row, col).value += val
+            wb.save(name)
+        except Exception as e:
+            app.logger.error(f"Таблицы {name} не существует ({e})")
+    elif method == CHECK:  # Проверить, что таблица существует, и добавить начальные значения, если нужно
+        try:
+            openpyxl.load_workbook(name)
+            return True
+        except:
+            wb = openpyxl.Workbook()
+            ws = wb.active
+            ws.title = 'Sheet1'
+        for i in args:
+            ws.append(i)
+        try:
+            wb.save(name)
+        except Exception as e:
+            app.logger.error(f"Произошла непредвиденная ошибка: {e}")
+        return False
+    else:
+        app.logger.error(f"Несуществующий метод {method}")
+
+
+def find_login_password_index(login, *, return_password=False):
+    try:
+        wb = openpyxl.load_workbook(f'{ACCOUNTS_XLSX_PATH}')
+        ws = wb['Sheet1']
+        if not return_password:
+            i = 1
+            for row in ws.iter_rows():
+                if row[0].value == login:
+                    return i
+                i += 1
+            return None
+        else:
+            for row in ws.iter_rows():
+                if row[0].value == login:
+                    return row[1].value
+            return None
+    except:
+        return None
+
+
+def change_ingredients(*args, allow_negative=False, enough_return=True, lack_return=False):
+    if not edit_excel(name=STORAGE_XLSX_PATH, method=CHECK):
+        for i in ingredient_code_transcript:
+            edit_excel(ingredient_code_transcript[i], PLACEHOLDER_STORAGE_ITEM_AMOUNT, name=STORAGE_XLSX_PATH)
+    wb = openpyxl.load_workbook(f'{STORAGE_XLSX_PATH}')
+    ws = wb['Sheet1']
+    edit_data = []  # [[row, col, val], ...]
+    for arg in args:
+        row_index = 1
+        for row in ws.iter_rows(values_only=True):
+            code = row[INGREDIENT_NAME_COLUMN_INDEX]
+            for k, v in ingredient_code_transcript.items():
+                if v == code:
+                    code = k
+                    break
+            if code == arg[INGREDIENT_CODE_ING_CHAR_INDEX]:
+                try:
+                    if not allow_negative and row[INGREDIENT_AMOUNT_COLUMN_INDEX] - int(arg[INGREDIENT_CODE_AMT_START_INDEX:]) < 0:
+                        app.logger.warning("Недостаточно ингредиентов на складе.")
+                        return lack_return
+                    else:
+                        edit_data.append([row_index, INGREDIENT_AMOUNT_COLUMN_INDEX + 1, -int(arg[INGREDIENT_CODE_AMT_START_INDEX:])])
+                except Exception as e:
+                    app.logger.error(f"Не правильный синтаксис индекса ингредиента. Доп. информация: {e}")
+            row_index += 1
+    for data in edit_data:
+        edit_excel(data[0], data[1], data[2], name=STORAGE_XLSX_PATH, method=MODIFY)
+    return enough_return
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        session['name'] = request.form.get('name', 'N/A')
-        session['surname'] = request.form.get('surname', 'N/A')
-        session['lastname'] = request.form.get('lastname', 'N/A')
-        session['age'] = request.form.get('age', 'N/A')
-        session['email'] = request.form.get('email', 'N/A')
-        session['phone'] = request.form.get('phone', 'N/A')
-        session['login'] = request.form.get('login', 'N/A')
-        session['password'] = request.form.get('password', 'N/A')
+        session['name'] = request.form.get('name')
+        session['surname'] = request.form.get('surname')
+        session['lastname'] = request.form.get('lastname')
+        session['age'] = request.form.get('age')
+        session['email'] = request.form.get('email')
+        session['phone'] = request.form.get('phone')
+        session['login'] = request.form.get('login')
+        session['password'] = request.form.get('password')
         session['order_number'] = ['A', 'B', 'C', 'D', 'E', 'F', 'G'][randint(0, 6)] + str(randint(1, 99))
         session['custom_pizzas_amt'] = 0
         session['total_cost'] = 0
@@ -93,6 +188,14 @@ def index():
         session['c_pizza_cost'] = 100
         session['ingreds_in_custom_pizza'] = []
         session['purchases'] = dict()
+        session['purchase_completed'] = False
+        if session.get("login"):
+            passwd = find_login_password_index(session.get("login"), return_password=True)
+            if os.path.exists(ACCOUNTS_XLSX_PATH) and passwd:
+                if not bcrypt.checkpw(session.get("password").encode("utf-8"), passwd.encode("utf-8")):
+                    session["login"] = None
+                    session["password"] = None
+                    return f"""Введён неверный пароль<br><br><a href="{url_for('index')}">Вернуться в меню регистрации</a><br><br><a href="{url_for('main_page')}">Продолжить без аккаунта</a>"""
         if test_info(session.get("name"), session.get("surname"), session.get("lastname"), session.get("age"),
                      session.get("phone"), session.get("email")):
             return redirect(url_for('main_page'))
@@ -107,12 +210,12 @@ def index():
 def main_page():
     update_purchases_data()
     menu_string = f"\n{' ' * 12}МЕНЮ{' ' * 12}\n{'-' * 30}\n"
-    for k, v in menu.items():
+    for k, v in MENU.items():
         menu_string += f"{k}: стоимость {v}\n"
     menu_string += "-" * 30 + '\n'
     if int(session.get("age")) >= 18:
         menu_string += "МЕНЮ ДЛЯ ВЗРОСЛЫХ\n"
-        for k, v in spec_menu.items():
+        for k, v in SPEC_MENU.items():
             menu_string += f"{k}: стоимость - {v} рублей\n"
         menu_string += "-" * 30
     order = f"Ваш заказ:\n{'-' * 30}\n{session.get('purchases_list')}\nИТОГО: {session.get('total_cost')} РУБЛЕЙ\n{'-' * 30}"
@@ -122,54 +225,63 @@ def main_page():
 @app.route("/order/add_item", methods=["POST", "GET"])
 def add_item():
     purchases = session.get('purchases')
-    item = request.form.get("item_to_add", "N/A").strip()
+    item = request.form.get("item_to_add", " ").strip()
+    if not change_ingredients(*food_ingreds.get(item, ("-0",))):
+        return f"""На складе закончились ингредиенты для этого блюда. Приносим извинения за доставленные неудобства.<br>Обратитесь за помощью к рабочему ресторана, или попробуйте заказать другое блюдо.<br><a href="{url_for('main_page')}">Вернуться в меню оформления заказа</a>"""
     try:
-        purchases[item][0] += menu[item]
+        purchases[item][0] += MENU[item]
         purchases[item][1] += 1
         session["purchases"] = purchases
         return redirect(url_for("main_page"))
     except:
         try:
-            purchases[item][0] += spec_menu[item]
+            purchases[item][0] += SPEC_MENU[item]
             purchases[item][1] += 1
             session["purchases"] = purchases
             return redirect(url_for("main_page"))
         except:
             pass
     try:
-        purchases[item] = [menu[item], 1]
+        purchases[item] = [MENU[item], 1]
         session["purchases"] = purchases
         return redirect(url_for("main_page"))
     except:
         if int(session.get("age")) >= 18:
             try:
-                purchases[item] = [spec_menu[item], 1]
+                purchases[item] = [SPEC_MENU[item], 1]
                 session["purchases"] = purchases
                 return redirect(url_for("main_page"))
             except:
-                return f"""В меню нет предмета {item}<br><a href="{url_for('main_page')}">Вернуться в меню оформелния заказа</a>"""
+                return f"""В меню нет предмета {item}<br><a href="{url_for('main_page')}">Вернуться в меню оформления заказа</a>"""
         else:
-            return f"""В детском меню нет предмета {item}<br><a href="{url_for('main_page')}">Вернуться в меню оформелния заказа</a>"""
+            return f"""В детском меню нет предмета {item}<br><a href="{url_for('main_page')}">Вернуться в меню оформления заказа</a>"""
+
+
+@app.route("/order/custom/modify_custom_pizzas_amt", methods=["POST", "GET"])
+def increase_custom_pizza_counter():
+    session['custom_pizzas_amt'] += 1
+    return redirect(url_for('custom_pizza'))
 
 
 @app.route("/order/custom/", methods=["POST", "GET"])
 def custom_pizza():
-    session['custom_pizzas_amt'] += 1
     ingreds = "-" * 30 + '\n'
-    for k, v in ingredients.items():
+    for k, v in INGREDIENTS.items():
         ingreds += f"{k}: стоимость {v}\n"
     ingreds_in_pizza = " - Тесто (100 рублей)\n"
     for i in session.get("ingreds_in_custom_pizza"):
-        ingreds_in_pizza += f" - {i} ({ingredients[i]} рублей)\n"
-        session['c_pizza_cost'] += ingredients[i]
+        ingreds_in_pizza += f" - {i} ({INGREDIENTS[i]} рублей)\n"
+        session['c_pizza_cost'] += INGREDIENTS[i]
     return render_template("custom_pizza.html", ingreds_in_pizza=ingreds_in_pizza, ingredients=ingreds, user_info=user_info_string())
 
 
 @app.route("/order/custom/add_ingredient", methods=["POST", "GET"])
 def custom_pizza_add_ingredient():
-    ingred = request.form.get("ingredient")
+    ingred = request.form.get("ingredient", " ").strip()
+    if not change_ingredients(*food_ingreds.get(ingred, ("-0",))):
+        return f"""На складе закончился этот ингредиент. Приносим извинения за доставленные неудобства.<br>Обратитесь за помощью к рабочему ресторана, или попробуйте добавить другой ингредиент.<br><a href="{url_for('custom_pizza')}">Вернуться в меню оформления заказа</a>"""
     try:
-        session['c_pizza_cost'] += ingredients[ingred]
+        session['c_pizza_cost'] += INGREDIENTS[ingred]
         session['ingreds_in_custom_pizza'].append(ingred)
         return redirect(url_for("custom_pizza"))
     except:
@@ -202,26 +314,57 @@ def pay():
 def pay_card():
     bonus_info = "Вам не были начислены бонусы, так как вы не зарегистрированы"
     if session.get("login") and session.get("password"):
+        if not session.get('purchase_completed'):
+            edit_excel(session.get("order_number"), user_info_string(), session.get("total_cost"),
+                       purchases_converted_to_string(),
+                       session.get("login"), "Оплачено картой")
+            passwd_i = find_login_password_index(session.get("login"))
+            if passwd_i:
+                edit_excel(passwd_i, 3, session.get('total_cost') // 10, name=ACCOUNTS_XLSX_PATH, method=MODIFY)
+            else:
+                edit_excel(session.get("login"),
+                           bcrypt.hashpw(session.get("password").encode("utf-8"), bcrypt.gensalt()).decode(),
+                           session.get('total_cost') // 10, name=ACCOUNTS_XLSX_PATH)
         bonus_info = f"На ваш аккаунт начислено {session.get('total_cost') // 10} бонусов"
+    else:
+        if not session.get('purchase_completed'):
+            edit_excel(session.get("order_number"), user_info_string(), session.get("total_cost"), purchases_converted_to_string(),
+                       "Оплачено картой")
     pay_info = "Оплата: картой"
+    session['purchase_completed'] = True
     return render_template("thanks_for_the_purchase.html", order_number=session.get('order_number'), bonus_info=bonus_info, user_info=user_info_string(), pay_info=pay_info)
 
 
 @app.route("/order/pay/cash", methods=["POST", "GET"])
 def pay_cash():
-    session["cash"] = request.form.get("cash", "N/A")
+    session["cash"] = request.form.get("cash")
     app.logger.debug(session.get("cash"))
     cash = 0
     try:
         cash = int(session.get("cash"))
     except:
         pass
-    app.logger.debug(cash)
     if cash and cash - int(session.get("total_cost")) >= 0:
         bonus_info = "Вам не были начислены бонусы, так как вы не зарегистрированы"
         if session.get("login") and session.get("password"):
+            if not session.get('purchase_completed'):
+                edit_excel(session.get("order_number"), user_info_string(), session.get("total_cost"),
+                           purchases_converted_to_string(),
+                           session.get("login"), "Оплачено наличными")
+                passwd_i = find_login_password_index(session.get("login"))
+                if passwd_i:
+                    edit_excel(passwd_i, 3, session.get('total_cost') // 10, name=ACCOUNTS_XLSX_PATH, method=MODIFY)
+                else:
+                    edit_excel(session.get("login"),
+                               bcrypt.hashpw(session.get("password").encode("utf-8"), bcrypt.gensalt()).decode(),
+                               session.get('total_cost') // 10, name=ACCOUNTS_XLSX_PATH)
             bonus_info = f"На ваш аккаунт начислено {session.get('total_cost') // 10} бонусов"
+        else:
+            if not session.get('purchase_completed'):
+                edit_excel(session.get("order_number"), user_info_string(), session.get("total_cost"),
+                           purchases_converted_to_string(), "Оплачено наличными")
         pay_info = f"Оплата: наличными\nСдача: {cash - session.get('total_cost')}"
+        session['purchase_completed'] = True
         return render_template("thanks_for_the_purchase.html", order_number=session.get('order_number'),
                                bonus_info=bonus_info, user_info=user_info_string(), pay_info=pay_info)
     else:
@@ -231,4 +374,18 @@ def pay_cash():
 @app.route("/order/pay/receipt", methods=["POST", "GET"])
 def receipt():
     update_purchases_data()
-    return render_template("receipt.html", date=datetime.date.today(), order_number=session.get("order_number"), client=user_info_string(), total_cost=session.get("total_cost"), purchases_list=session.get("purchases_list"), qr_filename=qr_code())
+    return render_template("receipt.html", date=datetime.date.today(), order_number=session.get("order_number"), client=user_info_string(), total_cost=session.get("total_cost"), purchases_list=session.get("purchases_list"), qr_url=url_for('static', filename=qr_code()))
+
+
+@app.route("/order/reset", methods=["POST", "GET"])
+def reset_data():
+    session['order_number'] = ['A', 'B', 'C', 'D', 'E', 'F', 'G'][randint(0, 6)] + str(randint(1, 99))
+    session['custom_pizzas_amt'] = 0
+    session['total_cost'] = 0
+    session['purchases_list'] = ""
+    session['c_pizza_cost'] = 100
+    session['ingreds_in_custom_pizza'] = []
+    session['purchases'] = dict()
+    session['purchase_completed'] = False
+    return redirect(url_for("main_page"))
+
